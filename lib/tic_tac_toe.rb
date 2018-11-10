97857e655afb9f948f9c30a65f1daa2bb6135694
Simon Thomas <simonpthomas89@gmail.com>
2018-11-10 17:55:09 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -128,13 +128,12 @@ def winner
 
 
     def play
-
+      while !over? && !won? && !draw?
+        turn
       if won? != false
         return "Congratulations #{winner}!"
       elsif draw? == true
         return "Cat's Game!"
-        while !over? && !won? && !draw?
-          turn
       end
     end
     end
